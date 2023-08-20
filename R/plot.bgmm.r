ff96b72dda49e2d2b453b26e3f2fb3f4f0463862
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2023-08-21 00:02:42 +0200

    requested by CRAN

--- R/plot.bgmm.r
+++ R/plot.bgmm.r
@@ -1,5 +1,5 @@
 plot.mModel <- function(x, ...) {
-#  if (x$d > 2) 
+#  if (x$d > 2)
 #    stop("PLOT SUPPORTS ONLY 1D and 2D data")
 
    map.class = NULL
@@ -13,35 +13,35 @@ plot.mModel <- function(x, ...) {
        map.class = NULL
    }
 
-  if (x$d == 1) 
+  if (x$d == 1)
     plot.1d(x$X, x$knowns, map.class, x, ...)
-  if (x$d > 1) 
+  if (x$d > 1)
     plot.2d(x$X, x$knowns, map.class, x, ...)
 }
 
 
-plot.2d <-function(X, knowns, map.class, bgmm2d, ...) {
+plot.2d <-function(x, knowns, map.class, bgmm2d, ...) {
   if (bgmm2d$d > 2) {
-      tmp     <- prcomp(rbind(X, knowns))
+      tmp     <- prcomp(rbind(x, knowns))
       bgmm2d$mu<- data.frame(bgmm2d$mu)
-      colnames(bgmm2d$mu) = colnames(X)
-      X       <- predict(tmp, X)[,1:2]
+      colnames(bgmm2d$mu) = colnames(x)
+      x       <- predict(tmp, x)[,1:2]
       knowns  <- predict(tmp, knowns)[,1:2]
       bgmm2d$mu<- predict(tmp, bgmm2d$mu)[,1:2]
       B       <- tmp$rotation[,1:2]
       cvar = array(0,dim=c(bgmm2d$k,2,2))
-      for (i in 1:(bgmm2d$k)) 
+      for (i in 1:(bgmm2d$k))
           cvar[i,,] <- t(B) %*% bgmm2d$cvar[1,,] %*% B
       bgmm2d$cvar <- cvar
    }
 
-   Xk = rbind(X, knowns)
-   
+   Xk = rbind(x, knowns)
+
    plot(Xk, type="n", ...)
    #
    # plot unknowns
-   if (!is.null(X))
-     points(X, pch=19, col=rgb(0,0,0,0.5), cex=0.5)
+   if (!is.null(x))
+     points(x, pch=19, col=rgb(0,0,0,0.5), cex=0.5)
    #
    # plot knowns
    if (!is.null(map.class)) {
@@ -60,17 +60,17 @@ plot.2d <-function(X, knowns, map.class, bgmm2d, ...) {
 }
 
 
-plot.1d <-function(X, knowns, map.class, bgmm1d, npoints=1000, addHlines=TRUE, ...) {
-   Xk = rbind(X, knowns)
-   
+plot.1d <-function(x, knowns, map.class, bgmm1d, npoints=1000, addHlines=TRUE, ...) {
+   Xk = rbind(x, knowns)
+
    densx = seq(min(Xk), max(Xk), length.out=npoints)
    densy = matrix(0, bgmm1d$k, npoints)
-   for (i in 1:bgmm1d$k) 
+   for (i in 1:bgmm1d$k)
       densy[i, ] = dnorm(densx, bgmm1d$mu[i,1], sqrt(bgmm1d$cvar[i,1,1]))
    #
    # plot densities
    matplot(densx, t(densy), col=1+(1:bgmm1d$k), lwd=2, type="l", ylab="density", ...)
-   if (addHlines) for (i in 1:bgmm1d$k) 
+   if (addHlines) for (i in 1:bgmm1d$k)
                      abline(v=bgmm1d$mu[i,1], lty=2)
    #
    # plot unknowns
@@ -78,6 +78,6 @@ plot.1d <-function(X, knowns, map.class, bgmm1d, npoints=1000, addHlines=TRUE, .
    #
    # plot knowns
    if (!is.null(map.class))
-     for (i in 1:length(unique(map.class))) 
+     for (i in 1:length(unique(map.class)))
         if (sum(map.class==i)>0) rug(knowns[map.class==i,], lwd=4, col=1+i, side=3)
 }
